7a52cc77cacaae6f77b7c0b195d630fc1da6f221
mbjelac <marko.bjelac@gmail.com>
2023-02-03 23:27:39 +0100

    Implement movement & collisions

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,10 +1,27 @@
 import Phaser from 'phaser';
-import { Coords, Level } from "./Level";
+import { Coords, Direction, Level } from "./Level";
+
 
 export default class Demo extends Phaser.Scene {
+
+  private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  // @ts-ignore
+  private walls: Phaser.Physics.Arcade.StaticGroup;
+  // @ts-ignore
+  private floors: Phaser.Physics.Arcade.StaticGroup;
+  // @ts-ignore
+  private player: Phaser.Physics.Arcade.Sprite;
+
+  private level: Level;
+
   constructor() {
     super('GameScene');
+
+    this.level = Level.random(103, 103);
+
   }
+
 
   preload() {
     this.load.image('wall', 'assets/tiles/wall.png');
@@ -14,39 +31,68 @@ export default class Demo extends Phaser.Scene {
 
   create() {
 
-    const level = Level.random(3, 3);
+    this.floors = this.physics.add.staticGroup();
+    this.walls = this.physics.add.staticGroup();
 
-    const floors = this.physics.add.staticGroup();
-    const walls = this.physics.add.staticGroup();
 
-    const center: Coords = {x: 4, y: 4};
+    const center: Coords = { x: 4, y: 4 };
 
     const levelOffset: Coords = {
-      x: center.x - level.start.x,
-      y: center.y - level.start.y,
+      x: center.x - this.level.start.x,
+      y: center.y - this.level.start.y,
     };
 
     const centerPixels = toPixels(center);
 
-    level.locations.forEach((row, y) => row.forEach((location, x) => {
+    this.level.locations.forEach((row, y) => row.forEach((location, x) => {
 
       const locationCoords = toPixels({
         x: levelOffset.x + x,
         y: levelOffset.y + y
       });
 
-      floors.create(locationCoords.x, locationCoords.y, 'floor');
+      this.floors.create(locationCoords.x, locationCoords.y, 'floor');
 
       location.things.forEach(thing => {
         if (thing.isWall) {
-          walls.create(locationCoords.x, locationCoords.y, 'wall');
+          this.walls.create(locationCoords.x, locationCoords.y, 'wall');
         }
       });
     }));
 
-    const player = this.physics.add.sprite(centerPixels.x, centerPixels.y, 'player');
+    this.player = this.physics.add.sprite(centerPixels.x, centerPixels.y, 'player');
 
-    this.physics.add.collider(player, walls);
+    this.cameras.main.startFollow(this.player);
+
+    this.physics.add.collider(this.player, this.walls);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.input.keyboard.on('keydown', (event: KeyboardEvent) => {
+
+      const direction = keyToDirection(event.key);
+
+      if (!!direction) {
+        this.move(direction);
+      }
+
+    });
+  }
+
+  private move(direction: Direction) {
+
+    const hasMoved = this.level.tryToMove(direction);
+
+    if (!hasMoved) {
+      return;
+    }
+
+    this.player.setX(this.player.x + tileSize * direction.deltaX);
+    this.player.setY(this.player.y + tileSize * direction.deltaY);
+  }
+
+  update() {
+
   }
 }
 
@@ -57,5 +103,18 @@ function toPixels(coords: Coords): Coords {
   return {
     x: coords.x * tileSize + tileCenterOffset,
     y: coords.y * tileSize + tileCenterOffset
+  }
+}
+
+function keyToDirection(eventKey: string): Direction | undefined {
+  switch (eventKey) {
+    case "ArrowUp":
+      return Direction.UP;
+    case "ArrowDown":
+      return Direction.DOWN;
+    case "ArrowRight":
+      return Direction.RIGHT;
+    case "ArrowLeft":
+      return Direction.LEFT;
   }
 }
--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -1,3 +1,40 @@
+export class Direction {
+
+  public static UP = new Direction(0, -1);
+  public static DOWN = new Direction(0, 1);
+  public static LEFT = new Direction(-1, 0);
+  public static RIGHT = new Direction(1, 0);
+
+  private constructor(public deltaX: number, public deltaY: number) {
+  }
+
+  public move(coords: Coords): Coords {
+    return {
+      x: coords.x + this.deltaX,
+      y: coords.y + this.deltaY
+    };
+  }
+}
+
+export interface Coords {
+  x: number,
+  y: number
+}
+
+export interface Location {
+
+  things: Thing[]
+}
+
+export interface Thing {
+
+  isWall: boolean
+}
+
+const defaultThing: Thing = {
+  isWall: false
+}
+
 export class Level {
 
   public static random(width: number, height: number): Level {
@@ -23,31 +60,42 @@ export class Level {
     return new Level(locations, start!);
   }
 
+  private playerLocation: Coords;
+
   constructor(
     public locations: Location[][],
     public start: Coords
   ) {
+    this.playerLocation = { ...start };
+  }
+
+  public tryToMove(direction: Direction): boolean {
+
+    const nextCoords = direction.move(this.playerLocation);
+
+    const nextLocation = this.getLocation(nextCoords);
+
+    const canMove = !!nextLocation && !nextLocation.things.some(thing => thing.isWall);
+
+    if (canMove) {
+      this.playerLocation = nextCoords;
+    }
+
+    return canMove;
+  }
+
+  private getLocation(coords: Coords): Location | undefined {
+    const row = this.locations[coords.y];
+
+    if (!row) {
+      return undefined;
+    }
+
+    return row[coords.x];
   }
 }
 
-export interface Coords {
-  x: number,
-  y: number
-}
 
-export interface Location {
-
-  things: Thing[]
-}
-
-export interface Thing {
-
-  isWall: boolean
-}
-
-const defaultThing: Thing = {
-  isWall: false
-}
 
 export function aWall(): Location {
   return { things: [{ ...defaultThing, isWall: true }] };
