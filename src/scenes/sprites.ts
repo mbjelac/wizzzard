262a3416469818f6f5dc6040f766bd64307eb78a
mbjelac <marko.bjelac@gmail.com>
2024-04-10 00:29:57 +0200

    Convert old sprite catalog to new

--- src/scenes/sprites.ts
+++ src/scenes/sprites.ts
@@ -1,10 +1,5 @@
 import { Coords } from "../engine/Level";
 
-export const SPRITE_CONFIGS = {
-  town: {
-    tree: spriteConfig({ x: 0, y: 0 })
-  }
-}
 
 export function spriteConfig(coords: Coords): SpriteConfig {
   return {
@@ -17,55 +12,105 @@ export const SPRITE_CONFIGS_BY_LOCATION: Map<string, SpriteConfig> = new Map();
 export const SPRITE_CONFIG_VOID: SpriteConfig = { tileCoords: { x: 0, y: 7 } };
 export const SPRITE_CONFIG_WIZARD: SpriteConfig = { tileCoords: { x: 0, y: 8 } };
 
-const sprites = new Map<string, SpriteConfig>([
-  ["void", { tileCoords: { x: 0, y: 7 } }],
-  ["wizard", { tileCoords: { x: 0, y: 8 } }],
-
-  ["town/wall-brown", { tileCoords: { x: 4, y: 3 } }],
-  ["town/tree", { tileCoords: { x: 0, y: 0 } }],
-  ["town/grass", { tileCoords: { x: 1, y: 0 } }],
-  ["forest/tree", { tileCoords: { x: 2, y: 0 } }],
-  ["forest/floor", { tileCoords: { x: 3, y: 0 } }],
-  ["town/brick", { tileCoords: { x: 4, y: 0 } }],
-  ["town/window-green", { tileCoords: { x: 5, y: 0 } }],
-  ["town/floor-wooden", { tileCoords: { x: 6, y: 0 } }],
-  ["forest/cabin-wooden-light-v", { tileCoords: { x: 7, y: 0 } }],
-  ["forest/cabin-wooden-dark-v", { tileCoords: { x: 8, y: 0 } }],
-  ["forest/cabin-wooden-light-h", { tileCoords: { x: 7, y: 1 } }],
-  ["forest/cabin-wooden-dark-h", { tileCoords: { x: 8, y: 1 } }],
-  ["town/pumpkin-static", { tileCoords: { x: 0, y: 1 } }],
-  ["town/pumpkin-mobile", { tileCoords: { x: 1, y: 1 } }],
-  ["town/fence-stone-light-h", { tileCoords: { x: 2, y: 1 } }],
-  ["town/fence-stone-light-v", { tileCoords: { x: 3, y: 1 } }],
-
-  ["town/door-green-yellow-knob-closed-v", { tileCoords: { x: 4, y: 1 } }],
-  ["town/door-green-yellow-knob-closed-h", { tileCoords: { x: 5, y: 1 } }],
-  ["town/door-green-yellow-knob-open-w", { tileCoords: { x: 4, y: 2 } }],
-  ["town/door-green-yellow-knob-open-s", { tileCoords: { x: 5, y: 2 } }],
-  ["town/door-green-yellow-knob-open-e", { tileCoords: { x: 6, y: 2 } }],
-  ["town/door-green-yellow-knob-open-n", { tileCoords: { x: 7, y: 2 } }],
-  ["town/door-green-yellow-keyhole-closed-v", { tileCoords: { x: 4, y: 4 } }],
-  ["town/door-green-yellow-keyhole-closed-h", { tileCoords: { x: 5, y: 4 } }],
-  ["town/door-green-yellow-keyhole-open-w", { tileCoords: { x: 4, y: 5 } }],
-  ["town/door-green-yellow-keyhole-open-s", { tileCoords: { x: 5, y: 5 } }],
-  ["town/door-green-yellow-keyhole-open-e", { tileCoords: { x: 6, y: 5 } }],
-  ["town/door-green-yellow-keyhole-open-n", { tileCoords: { x: 7, y: 5 } }],
-  ["town/door-brown-silver-keyhole-closed-v", { tileCoords: { x: 4, y: 6 } }],
-  ["town/door-brown-silver-keyhole-closed-h", { tileCoords: { x: 5, y: 6 } }],
-  ["town/door-brown-silver-keyhole-open-w", { tileCoords: { x: 4, y: 7 } }],
-  ["town/door-brown-silver-keyhole-open-s", { tileCoords: { x: 5, y: 7 } }],
-  ["town/door-brown-silver-keyhole-open-e", { tileCoords: { x: 6, y: 7 } }],
-  ["town/door-brown-silver-keyhole-open-n", { tileCoords: { x: 7, y: 7 } }],
-
-  ["town/herb-garden-green-blue", { tileCoords: { x: 0, y: 4 } }],
-  ["town/herb-garden-green-blue-red", { tileCoords: { x: 0, y: 5 } }],
-  ["town/herb-garden-green-yellow", { tileCoords: { x: 1, y: 4 } }],
-  ["town/herb-garden-green-yellow-orange", { tileCoords: { x: 1, y: 5 } }],
-
-  ["forest/rock-1", { tileCoords: { x: 0, y: 6 } }],
-  ["town/barrel-brown-water", { tileCoords: { x: 1, y: 6 } }],
-  ["town/barrel-brown-broken", { tileCoords: { x: 2, y: 6 } }],
-]);
+export const SPRITE_CONFIGS = {
+  town: {
+    wall: {
+      brown: spriteConfig({ x: 4, y: 3 })
+    },
+    tree: spriteConfig({ x: 0, y: 0 }),
+    grass: spriteConfig({ x: 1, y: 0 }),
+    brick: spriteConfig({ x: 4, y: 0 }),
+    window: {
+      green: spriteConfig({ x: 5, y: 0 }),
+    },
+    floor: {
+      wooden: spriteConfig({ x: 6, y: 0 }),
+    },
+    pumpkin: {
+      static: spriteConfig({ x: 0, y: 1 }),
+      mobile: spriteConfig({ x: 1, y: 1 }),
+    },
+    fence: {
+      stone: {
+        light: {
+          horizontal: spriteConfig({ x: 2, y: 1 }),
+          vertical: spriteConfig({ x: 3, y: 1 }),
+        }
+      }
+    },
+    door: {
+      greenYellow: {
+        knob: {
+          closed: {
+            vertical: spriteConfig({ x: 4, y: 1 }),
+            horizontal: spriteConfig({ x: 5, y: 1 }),
+          },
+          open: {
+            west: spriteConfig({ x: 4, y: 2 }),
+            south: spriteConfig({ x: 5, y: 2 }),
+            east: spriteConfig({ x: 6, y: 2 }),
+            north: spriteConfig({ x: 7, y: 2 }),
+          }
+        },
+        keyhole: {
+          closed: {
+            vertical: spriteConfig({ x: 4, y: 4 }),
+            horizontal: spriteConfig({ x: 5, y: 4 }),
+          },
+          open: {
+            west: spriteConfig({ x: 4, y: 5 }),
+            south: spriteConfig({ x: 5, y: 5 }),
+            east: spriteConfig({ x: 6, y: 5 }),
+            north: spriteConfig({ x: 7, y: 5 }),
+          }
+        }
+      },
+      brownSilver: {
+        keyhole: {
+          closed: {
+            vertical: spriteConfig({ x: 4, y: 6 }),
+            horizontal: spriteConfig({ x: 5, y: 6 }),
+          },
+          open: {
+            west: spriteConfig({ x: 4, y: 7 }),
+            south: spriteConfig({ x: 5, y: 7 }),
+            east: spriteConfig({ x: 6, y: 7 }),
+            north: spriteConfig({ x: 7, y: 7 }),
+          }
+        }
+      }
+    },
+    herbGarden: {
+      greenBlue: spriteConfig({ x: 0, y: 4 }),
+      greenBlueRed: spriteConfig({ x: 0, y: 5 }),
+      greenYellow: spriteConfig({ x: 1, y: 4 }),
+      greenYellowOrange: spriteConfig({ x: 1, y: 5 }),
+    },
+    barrel: {
+      brown: {
+        water: spriteConfig({x: 1, y: 6}),
+        broken: spriteConfig({x: 1, y: 6}),
+      }
+    }
+  },
+  forest: {
+    tree: spriteConfig({ x: 2, y: 0 }),
+    floor: spriteConfig({ x: 3, y: 0 }),
+    woodenWall: {
+      light: {
+        vertical: spriteConfig({ x: 7, y: 0 }),
+        horizontal: spriteConfig({ x: 7, y: 1 }),
+      },
+      dark: {
+        vertical: spriteConfig({ x: 8, y: 0 }),
+        horizontal: spriteConfig({ x: 8, y: 1 }),
+      },
+    },
+    rock: {
+      big: spriteConfig({x: 0, y: 6})
+    },
+  }
+}
 
 export interface SpriteConfig {
   tileCoords: Coords;
