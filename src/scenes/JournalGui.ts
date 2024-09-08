0a44735d71099c3863c5803113b66298affa6b9c
mbjelac <marko.bjelac@gmail.com>
2024-09-08 18:01:25 +0200

    Add go & cancel

--- src/scenes/JournalGui.ts
+++ src/scenes/JournalGui.ts
@@ -2,17 +2,18 @@ import Phaser from 'phaser';
 import { screen } from "../config";
 import { GAME } from "../engine/game";
 import depths from "./depths";
+import { Button } from "./widgets/Button";
+import toPixelCoords from "./toPixelCoords";
 
 export default class JournalGui extends Phaser.Scene {
 
-  // @ts-ignore
-  private background: Phaser.GameObjects.Sprite;
+  private background!: Phaser.GameObjects.Sprite;
 
-  // @ts-ignore
-  private title: Phaser.GameObjects.BitmapText;// @ts-ignore
+  private title!: Phaser.GameObjects.BitmapText;// @ts-ignore
 
-  // @ts-ignore
-  private page: Phaser.GameObjects.BitmapText;
+  private page!: Phaser.GameObjects.BitmapText;
+
+  private readonly goButton = new Button();
 
   constructor() {
     super("journal");
@@ -25,43 +26,68 @@ export default class JournalGui extends Phaser.Scene {
 
 
     this.events.on("create", async () => this.displayContent());
-    this.events.on("wake", async () => this.clearContent());
+    this.events.on("wake", async () => {
+      this.clearContent();
+      await this.displayContent();
+    });
+
+    this.goButton.preload(this);
   }
 
-  create(){
+  create() {
     this.background = this.physics.add
-      .sprite(screen.center.x, screen.center.y, "background")
-      .setDisplaySize(screen.size.width, screen.size.height);
+    .sprite(screen.center.x, screen.center.y, "background")
+    .setDisplaySize(screen.size.width, screen.size.height);
 
     this.title = this.add
-      .bitmapText(40, 50, "blackRobotoMicro", "")
-      .setMaxWidth( 300)
-      .setScale(4)
-      .setDepth(depths.info)
-      .setVisible(true);
+    .bitmapText(40, 50, "blackRobotoMicro", "")
+    .setMaxWidth(300)
+    .setScale(4)
+    .setDepth(depths.info)
+    .setVisible(true);
 
     this.page = this.add
-      .bitmapText(40, 100, "blackRobotoMicro", "")
-      .setMaxWidth( 496)
-      .setScale(4)
-      .setDepth(depths.info)
-      .setVisible(true);
+    .bitmapText(40, 100, "blackRobotoMicro", "")
+    .setMaxWidth(496)
+    .setScale(4)
+    .setDepth(depths.info)
+    .setVisible(true);
+
+    this.goButton.create(this);
+
+    this.input.keyboard.on('keydown', async (event: KeyboardEvent) => {
+
+      if (event.code === "Escape") {
+        this.scene.switch("errands")
+        return;
+      }
+    });
   }
 
-  private async displayContent(){
+  private async displayContent() {
 
     const errand = await GAME.getSelectedErrand();
 
-    if (errand === undefined){
+    if (errand === undefined) {
       return;
     }
 
     this.title.setText(errand.description.title);
     this.page.setText(errand.description.description);
+
+    this.goButton.show(
+    { x: 288, y: 670 },
+    "Go!",
+    () => {
+      GAME.goToErrand(errand.description.id);
+      this.scene.switch("level");
+    }
+    );
   }
 
   private clearContent() {
     this.title.setText("");
     this.page.setText("");
+    this.goButton.hide();
   }
 }
