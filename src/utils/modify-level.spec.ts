f03c814f5a6f6a793202124ea7b109f0be289c08
mbjelac <marko.bjelac@gmail.com>
2024-10-30 00:03:12 +0100

    Improve level modify tool

--- src/utils/modify-level.spec.ts
+++ src/utils/modify-level.spec.ts
@@ -1,27 +1,30 @@
 import { LevelDescription, LevelLocation } from "../game/level/LevelDescription";
 
 
-const levelDescriptionString = "";
-
-
 const emptyLevelLocation: LevelLocation = { things: [] };
 
 
 it("modify level", () => {
 
-  const level: LevelDescription = JSON.parse(levelDescriptionString);
+  const level: LevelDescription = require("./level.json");
 
   // crop top
   // level.matrix.splice(0, 28);
 
+  // crop bottom
+  level.matrix.splice(32, 37);
+
   // crop right
   // level.matrix.forEach(row => row.splice(27, 43));
+
+  // crop left
+  level.matrix.forEach(row => row.splice(0, 38));
 
   // insert tiles left
   // level.matrix.forEach(row => row.splice(0, 0, emptyLevelLocation));
 
   // insert tiles right
-  level.matrix.forEach(row => row.push(emptyLevelLocation));
+  // level.matrix.forEach(row => row.push(emptyLevelLocation));
 
   console.log(JSON.stringify(level));
 
